c97d1f16e8cd63dc31caf900bf3842d30ab948b6
Dillon L. Peterson <dillonpeterson@mac.com>
2018-05-16 18:35:41 -0500

    Green light for shell gRPC construction that does absolutely nothing

---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"golang.org/x/net/context"
+
+	pb "bitbucket.org/dillonlpeterson/shippy/consignment-service/proto/consignment"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+const (
+	port = ":50051"
+)
+
+// Repository is interface for ConsignmentRepository
+type Repository interface {
+	Create(*pb.Consignment) (*pb.Consignment, error)
+}
+
+// ConsignmentRepository is a dummy Repo
+type ConsignmentRepository struct {
+	consignments []*pb.Consignment
+}
+
+// Create created a consignment object.
+func (repo *ConsignmentRepository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
+	updated := append(repo.consignments, consignment)
+	repo.consignments = updated
+	return consignment, nil
+}
+
+// Service should implement all of the methods to satisfy the service
+// we defined in our protobuf definition. You can check the interface
+// in the generate code itself for the exact method signatures etc. to
+// give you a better idea
+type service struct {
+	repo Repository
+}
+
+func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment) (*pb.Response, error) {
+	// Save our consignment
+	consignment, err := s.repo.Create(req)
+	if err != nil {
+		return nil, err
+	}
+
+	// Return the matching 'Response' message we created in our protobuf definition.
+	return &pb.Response{Created: true, Consignment: consignment}, nil
+}
+
+func main() {
+	repo := &ConsignmentRepository{}
+
+	// Set-up our gRPC server.
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+
+	pb.RegisterShippingServiceServer(s, &service{repo})
+
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
